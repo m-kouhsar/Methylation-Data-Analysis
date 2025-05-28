8534b8384f22820b103b1430fc169df40db7b02b
Kouhsar <mk693@login02.cluster.local>
2025-05-28 09:58:01 +0100

    Updated

--- QC/EPICV1/RunQC.sh
+++ QC/EPICV1/RunQC.sh
@@ -3,13 +3,12 @@
 #SBATCH --export=ALL # export all environment variables to the batch job.
 #SBATCH -D . # set working directory to .
 #SBATCH -p mrcq # submit to the parallel test queue
-#SBATCH --time=2:00:00 # Maximum wall time for the job
+#SBATCH --time=4:00:00 # Maximum wall time for the job
 #SBATCH --nodes=1 # specify number of nodes.
 #SBATCH --ntasks-per-node=16 # specify number of processors.
 #SBATCH --mail-type=END # send email at job completion
 #SBATCH --mail-user=m.kouhsar@exeter.ac.uk # email address
-#SBATCH --output=%x.%j.out
-#SBATCH --error=%x.%j.err
+#SBATCH --output=QC.EPIC.V1.%j.out
 
 source $1
 
