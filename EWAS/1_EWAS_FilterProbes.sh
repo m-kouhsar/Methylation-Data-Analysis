078fb80ef1de3897d66f330deda317cf6824468b
Morteza Kouhsar <m.kouhsar@exeter.ac.uk>
2026-03-24 10:31:49 +0000

    Adding dir.create to filterprobes.R and some minor changes on filterprobes.sh

--- EWAS/1_EWAS_FilterProbes.sh
+++ EWAS/1_EWAS_FilterProbes.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-beta_file="/lustre/projects/Research_Project-191391/Morteza/KCL_FACS/Raw/KCL_FACS.betas.rds"
-OutputPrefix=/lustre/projects/Research_Project-191391/Morteza/KCL_FACS/Results/EWAS/KCL_FACS
+beta_file="$1"
+OutputPrefix="$2"
 
 ScriptDir=/lustre/projects/Research_Project-191391/Morteza/github/Methylation-Data-Analysis/EWAS
+#################################################################################################
 
 Rscript ${ScriptDir}/1_EWAS_FilterProbes.R "$beta_file" "$OutputPrefix" "$ScriptDir"
+
+echo "All done!"
